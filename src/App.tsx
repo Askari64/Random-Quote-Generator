ee43645252e4fbac599d91b52aa81abbd1df6c2f
Askari <askarir64@gmail.com>
2023-06-12 20:42:06 +0530

    project in work

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import {useState} from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 function App() {
 
+  const [quote, setQuote] = useState({text: '', author: ''});
+
   const [count, setCount] = useState(0)
-  
+
+  const [arrLen, setArrLen] = useState(0)
+
+  const quoteApi: any = 'https://type.fit/api/quotes';
+
   const handleClick = () => {
-    setCount(Math.ceil(Math.random()* 100));
+    setCount(Math.ceil(Math.random()*arrLen));
   }
+  
+
+  useEffect(() => {
+    axios.get(quoteApi)
+      .then((res) => {
+        console.log(res);
+         setArrLen(res.data.length)
+         setQuote(res.data[count])
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, [count])
 
   return (
     <>
-    <h1>Count: {count}</h1>
-    <button onClick={handleClick}>Press</button>
-    <p>Testing out vite</p>
+      <h1>Count: {count}</h1>
+      <h1>"{quote.text}"</h1>
+      <h3>~{quote.author}</h3>
+      <button onClick={handleClick}>Random Quote</button>
+      
     </>
   )
 }
